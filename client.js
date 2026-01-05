50f5f04740d29450a66a26fdddad0473d2da5071
shenronOp <sarojinisabat88@gmail.com>
2026-01-05 23:03:46 +0530

    bug fix

--- client.js
+++ client.js
@@ -1,7 +1,5 @@
 import Redis from 'ioredis'
 
-const redis = new Redis(process.env.REDIS_URL)
+const client = new Redis(process.env.REDIS_URL)
 
-await redis.set('key', 'ioredis')
-const result = await redis.get('key')
-console.log(result)
+module.exports(client)
